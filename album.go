ee414b7cd8155ffc06fc035f26b164a8b20f4d0f
nkress <kressnick25@gmail.com>
2025-02-22 12:43:00 +1000

    refactor: move route handlers to package

--- album.go
+++ album.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"album/internal/handler"
 	"album/internal/images"
+
 	"context"
 	"errors"
 	"fmt"
-	"html/template"
 	"log"
 	"log/slog"
-	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
 	"strings"
-	"sync"
 	"syscall"
 	"time"
 
@@ -51,24 +50,6 @@ type (
 		MinRefreshInterval int
 	}
 )
-
-type Index struct {
-	Title  string
-	Photos []string
-}
-
-type FileHolder struct {
-	Mu    sync.RWMutex
-	Files []string
-}
-
-// helper method to set files. Handles locking
-func (f *FileHolder) Set(files []string) {
-	f.Mu.Lock()
-	defer f.Mu.Unlock()
-
-	f.Files = files
-}
 
 func main() {
 	// --- Setup ---
@@ -120,7 +101,7 @@ func main() {
 	if fileLoadErr != nil {
 		slog.Error("failed to optimimise images: ", "err", fileLoadErr)
 	}
-	fileHolder := FileHolder{
+	fileHolder := handler.FileHolder{
 		Files: lo.Keys(fileEntries),
 	}
 	log.Printf("Found %d photos in %s", len(fileHolder.Files), conf.Home.Path)
@@ -184,67 +165,19 @@ func main() {
 	http.Handle("/public/", http.StripPrefix("/public/", publicServer))
 
 	// --- Routes ---
-	http.HandleFunc("/img/preview/{id}", func(w http.ResponseWriter, r *http.Request) {
-		requestFile := r.PathValue("id")
-		entry, ok := fileEntries[requestFile]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+	rootHandler := handler.RootHandler{
+		FileHolder: &fileHolder,
+	}
 
-		slog.Debug("", "requestFile", "/img/preview/"+requestFile, "responseFile", entry.GetPreview())
+	imageHandler := handler.ImageHandler{
+		FileEntries: fileEntries,
+	}
 
-		http.ServeFile(w, r, entry.GetPreview())
-	})
+	http.HandleFunc("/img/preview/{id}", imageHandler.Previews)
 
-	http.HandleFunc("/img/{id}", func(w http.ResponseWriter, r *http.Request) {
-		requestFile := r.PathValue("id")
-		entry, ok := fileEntries[requestFile]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
+	http.HandleFunc("/img/{id}", imageHandler.Images)
 
-		slog.Debug("", "requestFile", "/img/"+requestFile, "responseFile", entry.GetFullSize())
-
-		http.ServeFile(w, r, entry.GetFullSize())
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// FIXME is it bad to aquire a read lock all the time here?
-		// Maybe better to just have eventual consistency
-		// worst that could happen is the page loads with some dead image links, solved by refresh
-		fileHolder.Mu.RLock()
-		defer fileHolder.Mu.RUnlock()
-
-		f := fileHolder.Files
-		rand.NewSource(time.Now().UnixNano())
-		// shuffle the files slice
-		for i := range f {
-			j := rand.Intn(i + 1) // #nosec G404 -- secure random not required
-			f[i], f[j] = f[j], f[i]
-		}
-
-		data := Index{
-			Title:  "My Album",
-			Photos: f,
-		}
-
-		templateFile := "web/template/index.html"
-		t, err := template.ParseFiles(templateFile)
-		if err != nil {
-			slog.Error("Failed to parse template", "template", templateFile, "error", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		err = t.Execute(w, &data)
-		if err != nil {
-			slog.Error("Failed to execute template", "template", templateFile, "error", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-	})
+	http.HandleFunc("/", rootHandler.Index)
 
 	// --- Run ---
 	server := &http.Server{
